3e2bb263030d3e837b3a89358ff00b98fb8ba36b
Alary <168386521+AlaryGuzman@users.noreply.github.com>
2025-07-08 07:09:38 -0600

    Update login.js

--- js/login.js
+++ js/login.js
@@ -9,14 +9,21 @@ btnRegistrar.addEventListener('click', () => {
 btnIniciar.addEventListener('click', () => {
     contenedor.classList.remove("panel-activo");
 });
+
 // Validación Registro
 document.getElementById('btn-enviar-registro').addEventListener('click', () => {
     const nombre = document.querySelector('.registrar input[type="text"]').value.trim();
     const email = document.querySelector('.registrar input[type="email"]').value.trim();
     const password = document.querySelector('.registrar input[type="password"]').value.trim();
+    const correoValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (nombre === '' || email === '' || password === '') {
         alert('Por favor, completa todos los campos.');
+        return;
+    }
+
+    if (!correoValido.test(email)) {
+        alert('Introduce un correo electrónico válido.');
         return;
     }
 
@@ -25,22 +32,29 @@ document.getElementById('btn-enviar-registro').addEventListener('click', () => {
         return;
     }
 
+
     alert('Registro exitoso');
     contenedor.classList.remove("panel-activo");
-    
 });
 
 // Validación Ingreso
 document.getElementById('btn-enviar-login').addEventListener('click', () => {
     const email = document.querySelector('.ingresar input[type="email"]').value.trim();
     const password = document.querySelector('.ingresar input[type="password"]').value.trim();
+    const correoValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (email === '' || password === '') {
         alert('Por favor, completa todos los campos.');
         return;
     }
 
+    if (!correoValido.test(email)) {
+        alert('Introduce un correo electrónico válido.');
+        return;
+    }
+
     alert('Inicio de sesión exitoso');
-   window.location.href = 'https://alaryguzman.github.io/Portafolio/';
+    window.location.href = 'https://alaryguzman.github.io/Portafolio/';
 });
+
 
